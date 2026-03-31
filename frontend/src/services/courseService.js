a88b80aa22e1a6ebede7622fb0a994a496eadef2
jerememetan <129954878+jerememetan@users.noreply.github.com>
2026-03-31 13:44:40 +0800

    fixed courseService Paht

--- frontend/src/services/courseService.js
+++ frontend/src/services/courseService.js
@@ -5,7 +5,7 @@ const courseRes = await fetch("http://127.0.0.1:3017/api/courses");
   return courseArr;
 }
 export async function fetchCourseByCode(code){
-const courseRes = await fetch("http://127.0.0.1:3017/api/" + code);
+const courseRes = await fetch("http://127.0.0.1:3017/api/courses/" + code);
   const courseJson = await courseRes.json();
   const courseArr = courseJson.data?.Courses || [];
   return courseArr;
